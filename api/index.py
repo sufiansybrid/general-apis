d6d1b8ed272f75b0e90685b907689198e2126134
Sufian Uddin <sufian.uddin@sybrid.com>
2026-08-18 18:50:05 +0500

    Comment out Flask app execution block for future reference

--- api/index.py
+++ api/index.py
@@ -952,5 +952,5 @@ def get_ke_bill():
 # Run Flask App
 # ===========================
 
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, debug=False)
+# if __name__ == '__main__':
+#     app.run(host="0.0.0.0", port=5000, debug=False)
